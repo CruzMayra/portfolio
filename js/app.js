b43a11931d5cd0bb38d40285873fa26b82af608a
CruzMayra <velazquez.leonora@gmail.com>
2018-05-21 18:29:25 -0500

    creación de routing

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 /*---------- función que centraliza al resto de las funciones ----------*/
 function loadPage() {
   $('.load-landing').click(hideHome);
-  $('.view-venus').click(viewVenus)
  }
 
  const hideHome = (e) => {
@@ -16,14 +15,66 @@ function loadPage() {
   $('#landing').addClass('show');
  }
 
- const viewVenus = e => {
-   e.preventDefault();
-   page('/venus-ecommerce')
+ $('.view-venus').click(function(e){
+  e.preventDefault();
+  const pageRef = e.target.getAttribute('href');
+  page(pageRef);
+})
+
+page('/venus-ecommerce', e => {
+  getData(e.path);
+  
+})
+
+ function getData(dataProject) {
+   const project = projects.find( project => {
+     return project.id === dataProject;
+   })
+   paintProject(project);
  }
 
- page('/venus-ecommerce', () => {
+ function paintProject(project) {
    $('#content-me').empty();
-})
+   let templateProject = '';
+   templateProject += `<section id="projects">
+   <section class="jumbotron">
+     <div class="container text-center">
+       <h2 class="name">${project.name}</h2>
+       <img src="${project.image}" alt="">   
+     </div> 
+   </section>
+ 
+   <section class="container project-d">
+     <div class="row">
+       <div class="col-md-9">
+         <h3>Descripción del proyecto</h3>
+         <p>${project.description}</p>
+       </div>
+       <div class="col-md-3 details-project">
+         <div>
+           <h4>Tipo de proyecto</h4>
+           <p>${project.type}</p>
+         </div>
+         <div>
+           <h4>Qué hice :)</h4>
+           <p>${project.do}</p>
+         </div>
+         <div>
+           <h4>Tecnologías usadas</h4>
+           <p>${project.technology}</p>
+         </div>
+         <div>
+           <h4>Ver el proyecto</h4>
+           <p><a href="${project.demo}"><i class="fas fa-eye"></i></a></p>
+           <h4>Ver código</h4>
+           <p><a href="${project.code}"><i class="fab fa-github-alt"></i></a></p>
+         </div>
+       </div>
+     </div>
+   </section>
+ </section>`;
+ $('#content-me').append(templateProject); 
+ }
 
  $(document).ready(loadPage);
 
--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
-const projets = [
+const projects = [
   {
-    data:'venus',
+    id:'/venus-ecommerce',
     name: 'Venus e-commerce',
     image: 'assets/images/venus-02.jpg',
     description: 'E-commerce dirigo a mujeres jóvenes que disfrutan de un estilo en tendencia así como de las prendas sencillas y cómodas. Proyecto colaborativo que aprovecha el uso de la técnica SPA para proveer una interfaz sencilla e intuitiva, permite al usuario saber siempre en que sección se encuentra gracias al uso del routing. Aprovecha el uso de API para ofrecer una extensa variedad de productos (Etsy) y un método de pago sencillo (PayPal).',
@@ -11,7 +11,7 @@ const projets = [
     code: 'https://github.com/CruzMayra/venus-e-commerce'
   },
   {
-    data:'pokedex',
+    id:'pokedex',
     name: 'Pokédex',
     image: 'assets/images/pokeapi-02.png',
     description: 'Aplicación que emula la enciclopedia virtual que los entrenadores llevan consigo en la serie, para consultar las fichas de las diversas especies Pokémon. Proyecto individual que hace uso de una GrahpQL API para realizar una sola petición por medio de jQuery. Aprovecha el manejo del DOM para crear de manera dinámina los modales con la información particular de cada pokémon.',
@@ -22,7 +22,7 @@ const projets = [
     code: 'https://github.com/CruzMayra/pokedex'
   },
   {
-    data: 'ticket',
+    id: 'ticket',
     name: 'Ticket-hack',
     image: 'assets/images/phone-ticket-01.jpg',
     description: 'Plataforma que busca ofrecer un espacio para el intercambio de boletos, principalmente para conciertos, desde una comunidad segura y confiable. Ticket-hack es un proyecto colaborativo realizado durante la hackatón interna de Laboratoria. Utiliza Firebase como medio de autentificación e identificación del usuario. Hace uso de la API de Ticketmaster para mostrar los eventos más próximos en la ciudad.',
@@ -33,7 +33,7 @@ const projets = [
     code: 'https://github.com/CruzMayra/ticket-hack'
   },
   {
-    data: 'scribere',
+    id: 'scribere',
     name: 'Scribere: Write free, be free!!!',
     image: 'assets/images/network-02.jpg',
     description: 'Red social para los entusiastas de la escritura. Scribere permite al usuario crear sus propias historias, leer las aportaciones de otros usuarios y guardar como favoritas aquellas historias que más lo impactaron. Proyecto colaborativo que utiliza el feedback de los usuarios para su creación a fin de satisfacer sus necesidades. Scribere también hace uso de Firebase como medio de autentificación e identificación de los usuarios.',
@@ -44,7 +44,7 @@ const projets = [
     code: 'https://github.com/CruzMayra/scribere-social-network'
   },
   {
-    data: 'labcar',
+    id: 'labcar',
     name: 'LabCar',
     image: 'assets/images/lc-01.png',
     description: 'Proyecto individual que replica la página de Lab-Car con la utilización, en ese momento, por primera vez de un framework (Bootstrap) y la aplicación del responsive design.',
