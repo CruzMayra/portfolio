94bc043bac1631b5174221b6c384990e4a5d98e3
CruzMayra <velazquez.leonora@gmail.com>
2018-05-21 00:19:39 -0500

    creación de funciones de routing

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /*---------- función que centraliza al resto de las funciones ----------*/
 function loadPage() {
   $('.load-landing').click(hideHome);
+  $('.view-venus').click(viewVenus)
  }
 
  const hideHome = (e) => {
@@ -15,4 +16,15 @@ function loadPage() {
   $('#landing').addClass('show');
  }
 
+ const viewVenus = e => {
+   e.preventDefault();
+   page('/venus-ecommerce')
+ }
+
+ page('/venus-ecommerce', () => {
+   $('#content-me').empty();
+})
+
  $(document).ready(loadPage);
+
+ page.start({ hashbang: true })
